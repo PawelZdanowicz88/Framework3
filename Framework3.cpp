e95e2155bc7b89e0176af198932790576a3c525b
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2022-09-10 15:21:00 +0200

    First commit

---
+++ Framework3.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <string>
+struct address
+{
+    std::string city;
+    std::string street;
+    int home_number = 0;
+    int flat_number = 0;
+    int index_number = 0;
+};
+void print_address(address first_person)
+{
+    std::cout << first_person.city << std::endl;
+    std::cout << first_person.street << std::endl;
+    std::cout << first_person.home_number << std::endl;
+    std::cout << first_person.flat_number << std::endl;
+    std::cout << first_person.index_number << std::endl;
+
+}
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    address first_person;
+    std::cout << "Введите название города: ";
+    std::cin >> first_person.city;
+    std::cout << "Введите название улицы: ";
+    std::cin >> first_person.street;
+    std::cout << "Введите название дома: ";
+    std::cin >> first_person.home_number;
+    std::cout << "Введите название квартиры: ";
+    std::cin >> first_person.flat_number;
+    std::cout << "Введите индекс: ";
+    std::cin >> first_person.index_number;
+    print_address (first_person);
+    return 0;
+}
